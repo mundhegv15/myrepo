354f150942f830f00f8d71bdb778e3959e48b011
Ganesh <ergvmundhe@gmail.com>
2022-02-25 16:50:33 +0530

    parsed url

--- src/Digilogin.js
+++ src/Digilogin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useState}from 'react';
 import { useParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
@@ -6,23 +6,35 @@ import { useLocation } from 'react-router-dom';
 export default function Digilogin(props)
 {
 
+const [para , setparametr] = useState()
+
     const params = useParams()
     const productId = params.id;
     const search = useLocation().search;
   const token = new URLSearchParams(search).get('access_token');
-  const authcode = new URLSearchParams(search).get('authorization_code');
-  const name = new URLSearchParams(search).get('name');
+   const authcode = new URLSearchParams(search).get('authorization_code');
+   const name = new URLSearchParams(search).get('name');
+   const rtoken = new URLSearchParams(search).get('refresh_token');
+   const exptime = new URLSearchParams(search).get('expires_in');
+   
 
-    // const query = props.location.search;
-    return(
+function handleshow(){
+   
+setparametr(<p>
+    <h3>Name : {name}</h3>
+  <h3>Access_token  :{token}</h3>
+  <h3>Authorization_code : {authcode}</h3>
+  <h3>Refresh token : {rtoken}</h3>
+  <h3>Expires in : {exptime}</h3>
+  </p>);
+}   
+   
+   return(
 <>
 <h1>Digi locker login success...!</h1>
+<button onClick={handleshow}>ShowData</button>
 
-<div>
-Name : {name}
-access_token :{token}
-Authorization_code :{authcode}
-</div>
+<div>{para}</div>
 </>
     );
   
