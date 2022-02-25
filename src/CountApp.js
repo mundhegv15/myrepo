36a1f673a1c202ef635a6265ef8ebf272c352ee9
Ganesh <ergvmundhe@gmail.com>
2022-02-25 12:52:00 +0530

    added dom

--- src/CountApp.js
+++ src/CountApp.js
@@ -2,9 +2,16 @@ import React, { useState } from "react";
 import CountComponent from "./CountComponent";
 import './App.css'
 import Navbar from "./Navbar";
+import About from "./About";
+import { BrowserRouter as Router, Switch, Route, Link , Routes } from "react-router-dom";
+import refnum from "./refnum.mjs";
+let redirect_uri = "https://master.drdul8gu26jrd.amplifyapp.com/redirect";
+
+
 
 export default  function CountApp() {
 
+  const [client_id, setClientid] = useState(0);
     const [count, setCount] = useState(0);
     const [name, setname] = useState();
     const [aadhaar, setaadhaar] = useState();
@@ -13,12 +20,26 @@ export default  function CountApp() {
    const  handlenamechange = (e) => {
         setname(name);
     }
+    function setvalue(e)
+    {
+        setClientid(e.target.value);
+    
+    }
 
 return(
 <>
 <div> 
       <Navbar />
       <form>
+      <label>
+            Client Id :
+            <input
+              type={"text"}
+              name="client_id"
+              placeholder="Enter Client Id"
+              onChange={setvalue}
+            />
+          </label>
         <label>
           Name :
           <input type={"text"} name="fullname" value={name} placeholder="Full name" />
@@ -40,8 +61,25 @@ return(
             placeholder="0000-0000-0000"
           />
         </label>
-        
+ <button
+            type="submit"
+            name="digifetch"
+            formAction="https://182.72.231.115:8001/Requester/authorize"
+            // onClick={handleDigibtn}
+          >
+            Login with Digilocker
+          </button>
+          <input type={"hidden"} name="reference_id" value={refnum} />
+          <input type={"hidden"} name="redirect_uri" value={redirect_uri} />        
 </form>
+<Router >
+          <Routes>
+            <Route  path="/about" element = {<About/>}> </Route>
+            <Route path="/redirect"  element={<About /> }></Route>
+          </Routes>
+      </Router>
+
+
 </div>
 </>
 );
---
+++ src/refnum.mjs
@@ -0,0 +1,3 @@
+let refnum = Math.floor(Math.random() * 9000000000) + 1000000000;
+
+export default refnum;
