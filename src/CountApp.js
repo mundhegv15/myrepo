39c8a46748a97726e9c1988d1117db6ee233dc6d
Ganesh <ergvmundhe@gmail.com>
2022-02-28 19:17:20 +0530

    added refnum in route

--- src/CountApp.js
+++ src/CountApp.js
@@ -6,6 +6,7 @@ import About from "./About";
 import { BrowserRouter as Router, Switch, Route, Link , Routes } from "react-router-dom";
 import Digilogin from "./Digilogin";
 import Compo1 from "./Compo1.js";
+import refnum from "./refnum.mjs";
 
 
 
@@ -20,7 +21,7 @@ export default  function CountApp() {
        <Route  path="/" element = {<Compo1/>}> </Route>
             <Route  path="/home" element = {<Compo1/>}> </Route>
             <Route  path="/about" element = {<About/>}> </Route>
-            <Route path="/redirect"  element={<Digilogin />}> </Route>
+            <Route path = {`/${refnum}`}  element={<Digilogin />}> </Route>
        </Routes>
  </Router>
 </>
